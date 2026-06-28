bc247dc6f83a10a75fb95a81afba16182980a50d
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2026-06-28 15:28:24 +0200

    Adding unused arguments required by the function

--- plugins/larger_than_life/larger_than_life.ml
+++ plugins/larger_than_life/larger_than_life.ml
@@ -157,7 +157,7 @@ let make_rule rule_def =
       let next = next
     end)
 
-let run_for rule_def ~rows ~cols ~generations ~seed ~density ~topology ~output =
+let run_for rule_def ~rows ~cols ~generations ~seed ~density ~agents:_ ~topology ~output =
   let grid =
     Grid.create ~topology ~rows ~cols ()
   in
