aa8738955935062de9511b8cb991f304abbd4374
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2026-07-02 21:51:34 +0200

    Code simplification

--- io/metadata.ml
+++ io/metadata.ml
@@ -13,14 +13,12 @@
 
 type t = (string * string) list
 
-let empty =
-  []
+let empty = []
 
 let add key value metadata =
   (key, value) :: List.remove_assoc key metadata
 
-let get key metadata =
-  List.assoc_opt key metadata
+let get key metadata = List.assoc_opt key metadata
 
 let of_list xs =
   List.fold_left
@@ -29,23 +27,17 @@ let of_list xs =
     empty
     xs
 
-let to_list metadata =
-  List.rev metadata
+let to_list metadata = List.rev metadata
 
 let to_json metadata =
-  let escape s =
-    String.escaped s
-  in
-
   metadata
   |> List.rev
   |> List.map (fun (k, v) ->
       Printf.sprintf
         "  \"%s\": \"%s\""
-        (escape k)
-        (escape v))
+        (String.escaped k)
+        (String.escaped v))
   |> String.concat ",\n"
   |> Printf.sprintf "{\n%s\n}"
 
-let of_json json =
-  [ "raw_json", json ]
+let of_json json = [ "raw_json", json ]
