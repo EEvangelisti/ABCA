b070c977c0a36825d6ec0593e38de0dc64e7a76e
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2026-07-16 22:01:24 +0200

    Renaming files

---
+++ plugins/zoospores/empirical/zoospores_empirical_utils.ml
@@ -0,0 +1,15 @@
+(*
+ * ABCA (Agent-Based Cellular Automata)
+ * A modular simulation framework for discrete spatial systems,
+ * ranging from classical cellular automata to biologically inspired
+ * agent-based models.
+ *
+ * Copyright (c) 2026 Edouard Evangelisti
+ *
+ * Distributed under the MIT License.
+ * This software is provided "as is", without warranty of any kind.
+ * See the LICENSE file for details.
+ *)
+
+let clamp lo hi x = max lo (min hi x)
+let clamp01 x = clamp 0.0 1.0 x
