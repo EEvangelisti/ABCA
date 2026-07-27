26dcf433297d4567b1dfb5935bba0e22b646c57d
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2026-07-27 23:04:33 +0200

    Adding empirical zoospore model

---
+++ plugins/zoospores/examples/empirical.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd ../../..
+
+ROOT="plugins/zoospores"
+
+dune exec abca -- \
+  --mode run \
+  --model zoospores-empirical \
+  --rows 400 \
+  --cols 400 \
+  --generations 100 \
+  --agents 2000 \
+  --seed 42 \
+  --toroidal \
+  --plugin-arg INIT=CIRCLE \
+  --plugin-arg RADIUS=75 \
+  --plugin-arg PARAMS=$ROOT/empirical/data/P_nicotianae_local_parameters.csv \
+  --plugin-arg QUANTILES=$ROOT/empirical/data/P_nicotianae_empirical_quantiles.csv \
+  --plugin-arg MICRONS_PER_CELL=10 \
+  --out $ROOT/examples/P_nicotianae_empirical.bin
+
+dune exec abca -- \
+  --mode xml \
+  --model zoospores-empirical \
+  --input $ROOT/examples/P_nicotianae_empirical.bin \
+  --xml $ROOT/examples/P_nicotianae_empirical.xml
+
+dune exec abca -- \
+  --mode render \
+  --model zoospores-empirical \
+  --input $ROOT/examples/P_nicotianae_empirical.bin \
+  --gif P_nicotianae_empirical.gif \
+  --palette python-binary \
+  --background black \
+  --every 1 \
+  --fps 15
+
+cd $ROOT/examples
