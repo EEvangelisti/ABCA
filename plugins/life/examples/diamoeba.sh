b0ed144266ecebf23f60e644c231fdcb9eda1b28
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2026-07-27 22:52:25 +0200

    Adding example: diamoeba

---
+++ plugins/life/examples/diamoeba.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+cd ../../..
+
+dune exec abca -- \
+  --mode run \
+  --model diamoeba \
+  --rows 200 \
+  --cols 200 \
+  --toroidal \
+  --generations 200 \
+  --density 0.48 \
+  --seed 42 \
+  --out plugins/life/examples/diamoeba.bin
+
+dune exec abca -- \
+  --mode render \
+  --input plugins/life/examples/diamoeba.bin \
+  --gif diamoeba.gif \
+  --palette inferno \
+  --every 5
+
+cd plugins/life/examples
