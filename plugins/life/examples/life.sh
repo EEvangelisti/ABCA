92c105c7cdf8a9013f8e22372b0b1ab99d23a82d
Edouard Evangelisti <edouard.evangelisti@inrae.fr>
2026-07-27 22:42:54 +0200

    Example with CA: Life

---
+++ plugins/life/examples/life.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+cd ../../..
+
+dune exec abca -- \
+  --mode run \
+  --model life \
+  --rows 200 \
+  --cols 200 \
+  --toroidal \
+  --generations 200 \
+  --density 0.10 \
+  --seed 42 \
+  --out plugins/life/examples/life.bin
+
+dune exec abca -- \
+  --mode render \
+  --input plugins/life/examples/life.bin \
+  --gif life.gif \
+  --palette tol-muted
+
+cd plugins/life/examples
